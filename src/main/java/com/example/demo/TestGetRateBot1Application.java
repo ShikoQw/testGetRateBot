7a1f167ec8fb55f8495755f48a4c8a38caeda3a0
ShikoQw <qw_18@mail.ru>
2020-12-05 18:37:39 +0600

    committ

--- src/main/java/com/example/demo/TestGetRateBot1Application.java
+++ src/main/java/com/example/demo/TestGetRateBot1Application.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.botApi.Rate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class TestGetRateBot1Application {
 
--- src/main/java/com/example/demo/botApi/MainHandler.java
+++ src/main/java/com/example/demo/botApi/MainHandler.java
@@ -21,61 +21,67 @@ public class MainHandler {
     public SendMessage handleUpdate(Update update) {
         Message message = update.getMessage();
         //Проверка на наличия update
-        if (message != null && message.hasText()) {
-            String inputMsg = message.getText();
-            SendMessage replyMessage = new SendMessage();
-            String botState = "";
-            Rate rate = new Rate();
-            replyMessage.setChatId(message.getChatId().toString());
-
-            //Настройка клавиатуры
-            replyMessage.setReplyMarkup(replyKeyboardMarkup);
-            ArrayList<KeyboardRow> keyboard = new ArrayList<>();
-            KeyboardRow keyboardFirstRow = new KeyboardRow();
-            KeyboardRow keyboardSecondRow = new KeyboardRow();
-
-            replyKeyboardMarkup.setSelective(true);
-            replyKeyboardMarkup.setResizeKeyboard(true);
-            replyKeyboardMarkup.setOneTimeKeyboard(false);
-
-            keyboard.clear();
-            keyboardFirstRow.clear();
-            keyboardFirstRow.add("Курс Доллара \uD83C\uDDFA\uD83C\uDDF8");
-            keyboardFirstRow.add("Курс Евро \uD83C\uDDEA\uD83C\uDDFA");
-            keyboardFirstRow.add("Курс Рубля \uD83C\uDDF7\uD83C\uDDFA");
-            keyboardSecondRow.add("Помощь");
-            keyboard.add(keyboardFirstRow);
-            keyboard.add(keyboardSecondRow);
-            replyKeyboardMarkup.setKeyboard(keyboard);
-
-            //Обработка кнопок
-            switch (inputMsg) {
-                case "/start":
-                    replyMessage.setText("Приветствую!");
-                    replyMessage.setReplyMarkup(replyKeyboardMarkup);
-                    break;
-                case "Курс Доллара \uD83C\uDDFA\uD83C\uDDF8":
-                    botState = "Доллар";
-                    replyMessage.setText("курс доллара к тенге в народном банке на " + rate.getRateToUser(botState));
-                    replyMessage.setReplyMarkup(replyKeyboardMarkup);
-                    break;
-                case "Курс Евро \uD83C\uDDEA\uD83C\uDDFA":
-                    botState = "Евро";
-                    replyMessage.setText("курс евро к тенге в народном банке на " + rate.getRateToUser(botState));
-                    replyMessage.setReplyMarkup(replyKeyboardMarkup);
-                    break;
-                case "Курс Рубля \uD83C\uDDF7\uD83C\uDDFA":
-                    botState = "рубль";
-                    replyMessage.setText("курс российского рубля к тенге в народном банке на " + rate.getRateToUser(botState));
-                    replyMessage.setReplyMarkup(replyKeyboardMarkup);
-                    break;
-                default:
-                    replyMessage.setText("Нажмите на кнопку выбора валюты");
-                    replyMessage.setReplyMarkup(replyKeyboardMarkup);
-                    break;
-                }
-            return replyMessage;
+        if (message != null && !message.hasText()) {
+            SendMessage ErrorreplyMessage = new SendMessage();
+            ErrorreplyMessage.setChatId(message.getChatId().toString());
+            ErrorreplyMessage.setText("Неверный формат запроса");
+            return ErrorreplyMessage;
         }
+            if (message != null && message.hasText()) {
+                String inputMsg = message.getText();
+                SendMessage replyMessage = new SendMessage();
+                String botState = "";
+                Rate rate = new Rate();
+                replyMessage.setChatId(message.getChatId().toString());
+
+                //Настройка клавиатуры
+                replyMessage.setReplyMarkup(replyKeyboardMarkup);
+                ArrayList<KeyboardRow> keyboard = new ArrayList<>();
+                KeyboardRow keyboardFirstRow = new KeyboardRow();
+                KeyboardRow keyboardSecondRow = new KeyboardRow();
+
+                replyKeyboardMarkup.setSelective(true);
+                replyKeyboardMarkup.setResizeKeyboard(true);
+                replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+                keyboard.clear();
+                keyboardFirstRow.clear();
+                keyboardFirstRow.add("Курс Доллара \uD83C\uDDFA\uD83C\uDDF8");
+                keyboardFirstRow.add("Курс Евро \uD83C\uDDEA\uD83C\uDDFA");
+                keyboardFirstRow.add("Курс Рубля \uD83C\uDDF7\uD83C\uDDFA");
+                keyboardSecondRow.add("Помощь");
+                keyboard.add(keyboardFirstRow);
+                keyboard.add(keyboardSecondRow);
+                replyKeyboardMarkup.setKeyboard(keyboard);
+
+                //Обработка кнопок
+                switch (inputMsg) {
+                    case "/start":
+                        replyMessage.setText("Приветствую! Чтобы получить инфромацию о текущих курсах валют: нажмите на кнопку ниже");
+                        replyMessage.setReplyMarkup(replyKeyboardMarkup);
+                        break;
+                    case "Курс Доллара \uD83C\uDDFA\uD83C\uDDF8":
+                        botState = "Доллар";
+                        replyMessage.setText("курс доллара к тенге в народном банке на " + rate.getRateToUser(botState));
+                        replyMessage.setReplyMarkup(replyKeyboardMarkup);
+                        break;
+                    case "Курс Евро \uD83C\uDDEA\uD83C\uDDFA":
+                        botState = "Евро";
+                        replyMessage.setText("курс евро к тенге в народном банке на " + rate.getRateToUser(botState));
+                        replyMessage.setReplyMarkup(replyKeyboardMarkup);
+                        break;
+                    case "Курс Рубля \uD83C\uDDF7\uD83C\uDDFA":
+                        botState = "рубль";
+                        replyMessage.setText("курс российского рубля к тенге в народном банке на " + rate.getRateToUser(botState));
+                        replyMessage.setReplyMarkup(replyKeyboardMarkup);
+                        break;
+                    default:
+                        replyMessage.setText("Нажмите на кнопку выбора валюты");
+                        replyMessage.setReplyMarkup(replyKeyboardMarkup);
+                        break;
+                }
+                return replyMessage;
+            }
         return null;
     }
 }
