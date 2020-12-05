702625ba2fc97243d87d03270754afc5c7617556
ShikoQw <qw_18@mail.ru>
2020-12-06 05:22:07 +0600

    add google ping

--- src/main/java/com/example/demo/TestGetRateBot1Application.java
+++ src/main/java/com/example/demo/TestGetRateBot1Application.java
@@ -3,7 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.Schedules;
 
+@EnableScheduling
 @SpringBootApplication
 public class TestGetRateBot1Application {
 
